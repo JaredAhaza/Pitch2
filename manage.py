cb9eb4b038ba904189693a88a0e122cc0de497d2
JaredAhaza <jaredahazq@gmail.com>
2018-06-05 19:30:15 +0300

    Updated the manage file to run the server and enable connection to database

---
+++ manage.py
@@ -0,0 +1,33 @@
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User, Pitch, Comment
+
+
+
+app = create_app('production')
+
+manage = Manager(app)
+migrate = Migrate(app, db)
+
+manage.add_command('server', Server)
+manage.add_command('db', MigrateCommand)
+
+
+@manage.command
+def test():
+    """
+    Running the unit tests.
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manage.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
+
+
+if __name__ == '__main__':
+    manage.run()
